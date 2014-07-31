b31f337ab734f7ea9aea8a7d846a866f5f21b18a
Osvaldo Garcia <aoga88@gmail.com>
2014-07-31 12:22:27 -0500

    login and signup views

--- controllers/user.js
+++ controllers/user.js
@@ -6,3 +6,12 @@ exports.login = function(req, res)
 		res.render("dashboard/home");
 	}
 }
+
+exports.signup = function(req, res)
+{
+	if (typeof req.session.user === 'undefined') {
+		res.render("layouts/signup");
+	} else {
+		res.render("dashboard/home");
+	}
+}
